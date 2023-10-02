bf1648a11f1320b9451ca44fefad8980b26dba53
Victor Teo <victor.teo@fillmula.com>
2023-10-02 16:22:26 +0800

    update code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use wasm_bindgen::prelude::*;
+use teo_parser::{parse, generate_json_diagnostics};
 
 #[wasm_bindgen]
-pub fn lint(input: String) -> String {
-    //teo::lint(input)
-    "OK".to_owned()
+pub fn lint(file: String) -> String {
+    let (_, diagnostics) = parse(file);
+    generate_json_diagnostics(&diagnostics, true)
 }
