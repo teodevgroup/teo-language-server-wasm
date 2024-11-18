a50a56636b6d1f662fc3b9a267f9d4e66e8726b5
Victor Teo <victor.teo@fillmula.com>
2024-11-18 19:39:55 +0800

    update org name

--- scripts/fix.js
+++ scripts/fix.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
 const packageJson = JSON.parse(fs.readFileSync("pkg/package.json"))
-packageJson["name"] = "@teocloud/teo-language-server-wasm"
+packageJson["name"] = "@teodev/teo-language-server-wasm"
 delete packageJson["files"]
 fs.writeFileSync("pkg/package.json", JSON.stringify(packageJson, null, 4))
